2801d5d260cdb178d1bc6d6557a469c14e16894c
Nia447 <bayanshonhodoev@mail.ru>
2022-05-28 20:27:43 +0700

    .gitignore

--- Source/Game_Test_by_Nia477/Game_Test_by_Nia477.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Game_Test_by_Nia477.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Game_Test_by_Nia477, "Game_Test_by_Nia477" );
-
-DEFINE_LOG_CATEGORY(LogGame_Test_by_Nia477)
- 
